31b0f887141d1d930e36ff8c810753a28ee99442
Home <home@Roberts-MacBook-Pro.local>
2015-05-14 14:41:18 +0200

    test branch

---
+++ src/persons/persons.java
@@ -0,0 +1,21 @@
+package persons;
+
+public class persons {
+	String persons;
+	
+	public String getPersons() {
+		return persons;
+	}
+
+	public void setPersons(String persons) {
+		this.persons = persons;
+	}
+
+	public persons(){
+		
+	}
+	
+	
+	
+
+}
