54cc98dea1277ffab1f51014f3b6b69768d64ef2
Robert Stohr <gentdev@gmail.com>
2015-05-15 11:55:10 +0200

    Added JSoup

--- mvn/src/main/java/hello/HelloWorld.java
+++ mvn/src/main/java/hello/HelloWorld.java
@@ -1,6 +1,8 @@
-package main.java.hello;
+package hello;
 
 import org.joda.time.LocalTime;
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
 
 public class HelloWorld {
     public static void main(String[] args) {
@@ -8,5 +10,9 @@ public class HelloWorld {
 		System.out.println("The current local time is: " + currentTime);
 		Greeter greeter = new Greeter();
 		System.out.println(greeter.sayHello());
+
+		String html = "<html><head><title>First parse</title></head>"
+				+ "<body><p>Parsed HTML into a doc.</p></body></html>";
+		Document doc = Jsoup.parse(html);
 	}
 }
